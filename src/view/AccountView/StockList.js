9cdb82159840c9d4724b58b99cebd798b557b6d6
seungwon2 <dandelionsw2@ewhain.net>
2022-01-23 13:41:15 +0900

    :sparkles: feat : add account view

---
+++ src/view/AccountView/StockList.js
@@ -0,0 +1,58 @@
+/** @format */
+
+import { Link as RouterLink } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import IconButton from "@material-ui/core/IconButton";
+import Link from "@material-ui/core/Link";
+import { FavoriteBorder as FavoriteBorderIcon } from "@material-ui/icons";
+
+const StockList = (props) => {
+	const { dataArr } = props;
+
+	return (
+		<>
+			<Grid container spacing={4}>
+				{dataArr.map((el, i) => (
+					<Grid item key={i} xs={6} sm={4} md={3}>
+						<Card
+							sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
+							<CardMedia
+								sx={{ paddingTop: "100%", backgroundSize: "contain" }}
+								image={el.metadata.media}
+								title={el.metadata.title}
+							/>
+							<CardContent sx={{ flexGrow: 1 }}>
+								<Typography gutterBottom variant='h3' component='h2'>
+									{el.metadata.title}
+								</Typography>
+								<Typography>{el.metadata.extra} NEAR</Typography>
+								<Typography>{el.owner_id}</Typography>
+							</CardContent>
+							<CardActions>
+								<Grid container direction='row' justifyContent='space-between'>
+									<Link component={RouterLink} to={`/token/${el.token_id}`}>
+										<Button size='small' color='primary'>
+											View
+										</Button>
+									</Link>
+									<IconButton>
+										<FavoriteBorderIcon />
+									</IconButton>
+								</Grid>
+							</CardActions>
+						</Card>
+					</Grid>
+				))}
+			</Grid>
+		</>
+	);
+};
+
+export default StockList;
--- src/view/AccountView/index.js
+++ src/view/AccountView/index.js
@@ -1,12 +1,59 @@
+/** @format */
+
 import { NFTStorage, File } from "nft.storage";
+import { useEffect, useCallback } from "react";
+import { connect } from "react-redux";
+import { getNftList } from "../../redux/action";
+
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import IconButton from "@material-ui/core/IconButton";
+import Link from "@material-ui/core/Link";
+import StockList from "./StockList";
 
 const client = new NFTStorage({
-  token: process.env.REACT_APP_NFT_STORAGE_API_KEY,
+	token: process.env.REACT_APP_NFT_STORAGE_API_KEY,
 });
 
-const AccountView = () => {
-  console.log('NFTStorage', client);
-  return <h1>AccountView</h1>;
+console.log(client);
+
+const AccountView = (props) => {
+	console.log("p: ", props);
+	const { wallet, nftList, getNftList } = props;
+
+	const getNftListAccount = useCallback(async () => {
+		await getNftList();
+	}, [getNftList]);
+
+	useEffect(() => {
+		getNftListAccount();
+	}, [getNftListAccount]);
+
+	return (
+		<Container sx={{ paddingTop: "24px", paddingBottom: "24px" }} maxWidth='md'>
+			<Typography gutterBottom variant='h3' component='h2'>
+				마이페이지
+			</Typography>
+			<Grid container spacing={4}>
+				<StockList dataArr={nftList} />
+			</Grid>
+		</Container>
+	);
 };
 
-export default AccountView;
+const mapStateToProps = (state) => ({
+	wallet: state.app.wallet,
+	nftList: state.app.nftList,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+	getNftList: () => dispatch(getNftList()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AccountView);
