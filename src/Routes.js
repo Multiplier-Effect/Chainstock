14e953e35ce7146aa4bf061166031e56295b55c3
seungwon2 <dandelionsw2@ewhain.net>
2022-03-03 16:15:56 +0900

    :sparkles: feat : add new router path

--- src/Routes.js
+++ src/Routes.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import { Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import RouteWithLayout from "./utils/RouteWithLayout";
@@ -7,60 +9,67 @@ import DashboardView from "./view/DashboardView";
 import MainLayout from "./layouts/MainLayout";
 import MarketView from "./view/MarketPubView";
 import TokenView from "./view/TokenView";
+import MainView from "./view/MainView";
 import CreateTokenView from "./view/CreateTokenView";
 
 const Routes = (props) => {
-  const { wallet, auth } = props;
+	const { wallet, auth } = props;
 
-  console.log(auth);
+	console.log(auth);
 
-  if (wallet ? wallet.isSignedIn() : false) {
-    return (
-      <Switch>
-        <RouteWithLayout
-          path="/dashboard"
-          exact
-          component={DashboardView}
-          layout={DashboardLayout}
-        />
-        <RouteWithLayout
-          path="/account"
-          exact
-          component={AccountView}
-          layout={DashboardLayout}
-        />
-        <RouteWithLayout
-          path="/create"
-          exact
-          component={CreateTokenView}
-          layout={DashboardLayout}
-        />
-        <RouteWithLayout
-          path="/token/:id"
-          exact
-          component={TokenView}
-          layout={DashboardLayout}
-        />
-        <Redirect exact to="/dashboard" />
-      </Switch>
-    );
-  }
-  return (
-    <Switch>
-      <RouteWithLayout
-        path="/market"
-        exact
-        component={MarketView}
-        layout={MainLayout}
-      />
-      <Redirect exact to="/market" />
-    </Switch>
-  );
+	if (wallet ? wallet.isSignedIn() : false) {
+		return (
+			<Switch>
+				<RouteWithLayout
+					path='/'
+					exact
+					component={DashboardView}
+					layout={DashboardLayout}
+				/>
+				<RouteWithLayout
+					path='/market'
+					exact
+					component={DashboardView}
+					layout={DashboardLayout}
+				/>
+				<RouteWithLayout
+					path='/mypage'
+					exact
+					component={AccountView}
+					layout={DashboardLayout}
+				/>
+				<RouteWithLayout
+					path='/create'
+					exact
+					component={CreateTokenView}
+					layout={DashboardLayout}
+				/>
+				<RouteWithLayout
+					path='/token/:id'
+					exact
+					component={TokenView}
+					layout={DashboardLayout}
+				/>
+				<Redirect exact to='/dashboard' />
+			</Switch>
+		);
+	}
+	return (
+		<Switch>
+			<RouteWithLayout
+				path='/market'
+				exact
+				component={MarketView}
+				layout={MainLayout}
+			/>
+			<Redirect exact to='/market' />
+		</Switch>
+	);
 };
 
 const mapStateToProps = (state) => ({
-  wallet: state.app.wallet,
-  auth: state.app.auth,
+	wallet: state.app.wallet,
+	auth: state.app.auth,
 });
 
 export default connect(mapStateToProps)(Routes);
